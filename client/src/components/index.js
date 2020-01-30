6817f1557e05d003120eabf29ebb045c2b0d0fe6
alex.paclin <alex.paclin@gmail.com>
2020-01-30 14:34:02 +0500

    lesson5 more dumb components 57:20

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,10 +2,16 @@ import App from './app';
 import ChatWrapper from './chat-wrapper';
 import UserItem from './user-item';
 import PeopleSearch from './people-search';
+import ChatHeader from './chat-header';
+import ChatControl from './chat-control';
+import MessageItem from './message-item';
 
 export default App;
 export {
 	ChatWrapper,
 	UserItem,
-	PeopleSearch
+	PeopleSearch,
+	ChatHeader,
+	ChatControl,
+	MessageItem
 }
---
+++ client/src/components/message-item.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class MessageItem extends Component {
+	render() {
+		return (
+			<li class="clearfix">
+				<div class="message-data align-right">
+					<span class="message-data-time" >10:10 AM, Today</span> &nbsp; &nbsp;
+					<span class="message-data-name" >Olia</span> <i class="fa fa-circle me"></i>
+				</div>
+				<div class="message other-message float-right">
+					Hi Vincent, how are you? How is the project coming along?
+				</div>
+			</li>
+		)
+	}
+}
